023337e461f4f5b7e0f68bcc3d2025a7d5893a72
TheVaultCI236 <50559307+TheVaultCI236@users.noreply.github.com>
2019-05-13 00:17:44 +0100

    Add files via upload

---
+++ puzzle4.java
@@ -0,0 +1,17 @@
+
+public class puzzle4 {
+
+	
+	public static void main() {
+		
+		boolean securityAccess = true; 
+
+		int codeAccess = 125432;
+		
+		
+		// Multiply the code access variable
+		// by 7 and then re-submit through the
+		// print screen. 
+		
+	}
+}
